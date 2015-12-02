bdd8e49ee2f0763c64cf791d0fe0265fadd65b61
armartin <armartin@stanford.edu>
2015-12-01 20:07:39 -0500

    refactored map interpolator

--- makeMap_refactor.py
+++ makeMap_refactor.py
@@ -49,14 +49,14 @@ def full_map(chr, genmap, bim, map_bim):
             yield final_checks([chr, rsid, str(proportion), phys_pos, a0, a1])
             bim_line = bim.readline().strip().split()
             (rsid, phys_pos, a0, a1) = (bim_line[1], bim_line[3], bim_line[4], bim_line[5])
-        print 'paragraph 1'
         current_args = [phys_pos, start_bp, end_bp, rsid, bim, start_cM, end_cM, genmap, chr, a0, a1]
         while True:
-            print current_args
+            #print current_args
             current_args, to_write = check_conditions(current_args)
             
             if to_write is not None:
                 yield final_checks(to_write)
+                break
             if current_args is None:
                 break
 
