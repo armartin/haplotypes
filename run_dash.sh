bab89dfef277c811602b214ec86c097fb4383e9f
Alicia Martin <armartin@broadinstitute.org>
2016-03-23 13:49:32 -0400

    added cumulative germline calc + distance

--- run_dash.sh
+++ run_dash.sh
@@ -3,7 +3,7 @@
 #$ -q long
 #$ -t 1-22
 #$ -N dash
-#$ -l m_mem_free=64G
+#$ -l m_mem_free=96G
 #$ -w e
 #$ -e /home/unix/armartin/finrisk/haplotypes/germline/logs
 #$ -o /home/unix/armartin/finrisk/haplotypes/germline/logs
@@ -12,17 +12,14 @@ MATCH=$1
 FAM=$2
 OUT=$3
 
+THIS_MATCH=`echo $MATCH | perl -pi -e "s/chr\d+/chr${SGE_TASK_ID}/g"`
+THIS_OUT=`echo $OUT | perl -pi -e "s/chr\d+/chr${SGE_TASK_ID}/g"`
+
 if [[ $MATCH == *gz ]];
 then
-	zcat $MATCH | \
-	cut -f 1,2,4,10,11 | \
-	dash_cc $FAM
-	$OUT
+	zcat $THIS_MATCH | cut -f 1,2,4,10,11 | dash_cc $FAM $THIS_OUT
 else
-	cat $MATCH | \
-	cut -f 1,2,4,10,11 | \
-	dash_cc $FAM \
-	$OUT
+	cat $THIS_MATCH | cut -f 1,2,4,10,11 | dash_cc $FAM $THIS_OUT
 fi
 
-gzip ${OUT}.clst
+gzip ${THIS_OUT}.clst
