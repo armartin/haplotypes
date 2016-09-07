d85179314d2333e0f29ca6e22a25633d35fa702d
armartin <armartin@stanford.edu>
2016-09-06 21:46:36 -0400

    added regional ibd calculation

--- estimate_kinship.R
+++ estimate_kinship.R
@@ -1,1 +1,23 @@
 library(GENESIS)
+library(SNPRelate)
+
+# Create GDS file (only do once)
+snpgdsBED2GDS(bed.fn='/home/unix/armartin/finrisk/data/popgen_genotypes/imputed_data/Engagex_Egfext_Migraine_FinnishCardio_FTC_geno05_maf05_ld50.bed',
+              bim.fn='/home/unix/armartin/finrisk/data/popgen_genotypes/imputed_data/Engagex_Egfext_Migraine_FinnishCardio_FTC_geno05_maf05_ld50.bim',
+              fam.fn='/home/unix/armartin/finrisk/data/popgen_genotypes/imputed_data/Engagex_Egfext_Migraine_FinnishCardio_FTC_geno05_maf05_ld50.fam',
+              out.gdsfn='/home/unix/armartin/finrisk/data/popgen_genotypes/imputed_data/Engagex_Egfext_Migraine_FinnishCardio_FTC_geno05_maf05_ld50.gds')
+
+geno <- snpgdsOpen('/home/unix/armartin/finrisk/data/popgen_genotypes/imputed_data/Engagex_Egfext_Migraine_FinnishCardio_FTC_geno05_maf05_ld50.gds')
+
+geno <- GdsGenotypeReader(filename = "/home/unix/armartin/finrisk/data/popgen_genotypes/imputed_data/Engagex_Egfext_Migraine_FinnishCardio_FTC_geno05_maf05_ld50.gds")
+genoData <- GenotypeData(geno)
+mypcair <- pcair(geno)
+KINGmat <- snpgdsIBDKING(geno)
+
+iids <- getScanID(geno)
+
+# create matrix of KING estimates
+KINGmat <- king2mat(file.kin0 = "/home/unix/armartin/finrisk/data/popgen_genotypes/imputed_data/Engagex_Egfext_Migraine_FinnishCardio_FTC_geno05_maf05_ld50.kin0", 
+                    file.kin = "/home/unix/armartin/finrisk/data/popgen_genotypes/imputed_data/Engagex_Egfext_Migraine_FinnishCardio_FTC_geno05_maf05_ld50.kin", 
+                    iids = iids)
+mypcair <- pcair(genoData = genoData, kinMat = KINGmat, divMat = KINGmat)
---
+++ ibd_region.py
@@ -0,0 +1,116 @@
+"""
+pull out haplotypes overlapping a region
+write haplotype lengths for pairs of individuals within that region vs 
+"""
+import argparse
+import gzip
+from collections import defaultdict
+import re
+
+def open_file(filename):
+    if filename.endswith('gz'):
+        my_file = gzip.open(filename)
+    else:
+        my_file = open(filename)
+    return my_file
+
+def header_dict(my_header):
+    header = dict(zip(my_header, range(len(my_header))))
+    return(header)
+
+def main(args):
+    # read all ind info
+    ind_info = open_file(args.ind_info)
+    info_header = {}
+    header = ind_info.readline().strip().split(',')
+    header = header_dict(header)
+    geno_exome = {}
+    for line in ind_info:
+        line = line.strip().split(',')
+        geno_exome[line[header['ID2']]] = line[header['exome_id']]
+    
+    # read vcf info
+    vcf = open_file(args.vcf)
+    vcf_dict = defaultdict(dict)
+    snp_order = [] # store order to print snps in
+    chr_snps = {} # only go through each chromosome haps once
+    vcf_inds = set()
+    for line in vcf:
+        line = line.strip().split()
+        if line[0].startswith('##'):
+            pass
+        elif line.startswith('#CHROM'):
+            vcf_header = header_dict[line]
+            for i in line[9:len(line)]:
+                vcf_inds.add(i)
+        else:
+            snp_id = '_'.join(line[vcf_header['CHROM']], line[vcf_header['POS']], line[vcf_header['REF']], line[vcf_header['ALT']])
+            snp_order.append(snp_id)
+            chr_snps[line[vcf_header['CHROM']]] = [int(line[vcf_header['POS']]), line[vcf_header['REF']], line[vcf_header['ALT']]]
+            for ind in line[9:len(line)]:
+                vcf_dict[snp_id][ind] = ind.split(':')[0] #store genotypes for every pos and individual
+                
+    # read all haplotype info
+    snp_tot = default_dict(dict) # pos -> geno -> sorted pairs
+    snp_num = default_dict(dict) # pos -> geno -> num pairs
+    snp_len = default_dict(dict) # pos -> geno -> lengths list
+    for chrom in chr_snps:
+        haps = open_file('chr\d', 'chr' + chrom, args.haps)
+        for line in haps:
+            line = line.strip().split()
+            ind1 = line[1].split('.')[0]
+            ind2 = line[3].split('.')[0]
+            if geno_exome[ind1] in vcf_inds and geno_exome[ind2] in vcf_inds:
+                for snp in chr_snps[chrom]:
+                    pos = snp[0]
+                    snp_id = chrom + '_' + '_'.join(chr_snps[chrom])
+                    if pos >= int(line[5]) and pos <= int(line[6]):
+                        # need to check if inds share snp
+                        if vcf_dict[snp_id][ind1] == vcf_dict[snp_id][ind2]:
+                            if snp_id in snp_num and vcf_dict[snp_id][ind1] in snp_num[snp_id]:
+                                snp_num[snp_id][vcf_dict[snp_id][ind1]] += 1
+                                snp_len[snp_id][vcf_dict[snp_id][ind1]].append(line[10])
+                            else:
+                                snp_num[snp_id][vcf_dict[snp_id][ind1]] = 1
+                                snp_len[snp_id][vcf_dict[snp_id][ind1]] = [line[10]]
+                    ###NOTE: this won't work in non-Finns where it's not guaranteed that some individuals won't share haplotypes
+                    if vcf_dict[snp_id][ind1] == vcf_dict[snp_id][ind2]:
+                        if snp_id in snp_tot and vcf_dict[snp_id][ind1] in snp_tot[snp_id]:
+                            snp_tot[snp_id][vcf_dict[snp_id][ind1]].add(sorted([ind1, ind2]))
+                        else:
+                            snp_tot[snp_id][vcf_dict[snp_id][ind1]] = sorted([ind1, ind2])
+                
+    out = gzip.open(args.out, 'w')
+    out.write('\t'.join(['chr', 'pos', 'ref', 'alt', 'rr_tot', 'hh_tot', 'aa_tot', 'rr_haps', 'hh_haps', 'aa_haps', 'rr_len', 'hh_len', 'aa_len']))
+    possible_genos = ['0/0', '0/1', '1/1']
+    for snp in snp_order:
+        out.write('\t'.join(snp.split('_')) + '\t')
+        for geno in possible_genos:
+            if geno in snp_tot:
+                out.write(str(len(snp_tot[geno])) + '\t')
+            else:
+                out.write('0\t')
+        for geno in possible_genos:
+            if geno in snp_num[snp]:
+                out.write(str(snp_num[geno]) + '\t')
+            else:
+                out.write('0\t')
+        for geno in possible_genos:
+            if geno in snp_len[snp]:
+                out.write(str(snp_num[geno]) + '\t')
+            else:
+                out.write('0\t')
+        out.write('\n')
+    # print overlapping inds present
+    # print number of pairwise haps shared between inds
+    # print new inds present
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Parse some args')
+    parser.add_argument('--haps')
+    parser.add_argument('--ind_info')
+    parser.add_argument('--vcf')
+    parser.add_argument('--out')
+    
+    args = parser.parse_args()
+    main(args)
