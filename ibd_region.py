5e3ea8afa27112ed411e0fb91fba4c3684af8b59
armartin <armartin@stanford.edu>
2016-09-06 22:19:29 -0400

    added regional ibd calculation

--- ibd_region.py
+++ ibd_region.py
@@ -50,7 +50,7 @@ def main(args):
             snp_order.append(snp_id)
             chr_snps[line[vcf_header['#CHROM']]] = [int(line[vcf_header['POS']]), line[vcf_header['REF']], line[vcf_header['ALT']]]
             for ind in range(9, len(header_order)):
-                vcf_dict[snp_id][header_order[ind]] = line.split(':')[0] #store genotypes for every pos and individual
+                vcf_dict[snp_id][header_order[ind]] = line[ind].split(':')[0] #store genotypes for every pos and individual
                 
     # read all haplotype info
     snp_tot = defaultdict(dict) # pos -> geno -> sorted pairs
